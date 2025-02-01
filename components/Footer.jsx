819d860d08706eae36c0e889c68ba3c0f9ba2de0
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-02-01 04:17:00 +0200

    footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className=" flex flex-col md:flex-row items-center border-t justify-center gap-2 md:gap-10  text-center md:py-6 py-3 mt-10">
-      <div className="flex gap-6 text-sm md:gap-3 text-blue-700 font-medium">
+      <div className="flex gap-6 text-base md:gap-3 text-blue-700 font-medium">
         <Link href="/terms-of-use">Terms of Use</Link>
         <Link href="/privacy-policy">Privacy Policy</Link>
       </div>

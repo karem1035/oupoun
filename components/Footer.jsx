fe21eb1ca413a4f042e57cccd2cff2311fd5abae
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-02-01 02:51:31 +0200

    policy

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className=" flex flex-col md:flex-row items-center border-t justify-center gap-2 md:gap-10  text-center md:py-6 py-3 mt-10">
       <div className="flex gap-6 text-sm md:gap-3 text-blue-700 font-medium">
         <Link href="/terms-of-use">Terms of Use</Link>
-        <Link href="/privacy">Privacy Policy</Link>
+        <Link href="/privacy-policy">Privacy Policy</Link>
       </div>
       <p className="text-sm">Copyright © {new Date().getFullYear()} Oupoun.</p>
     </footer>
---
+++ pages/privacy-policy.jsx
@@ -0,0 +1,220 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="flex flex-col items-center justify-center p-6">
+      <div className="text-start leading-loose text-lg font-medium lg:w-[50%]">
+        <h1 className="md:text-3xl text-2xl md:my-5 mt-5 font-bold">
+          Oupoun Privacy & Policy
+        </h1>
+
+        <h2 className="text-2xl font-bold mt-8 mb-4">Privacy Policy</h2>
+        <p className="mb-4">
+          This privacy policy applies to the Oupoun app (hereby referred to as{' '}
+          <strong>"Application"</strong>) for mobile devices that was created by{' '}
+          <strong>Opal World</strong> (hereby referred to as{' '}
+          <strong>"Service Provider"</strong>) as a Free service. This service
+          is intended for use <strong>"AS IS"</strong>.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">
+          Information Collection and Use
+        </h3>
+        <p className="mb-4">
+          The Application collects information when you download and use it.
+          This information may include information such as:
+        </p>
+        <ul className="list-disc pl-8 mb-4">
+          <li>Your device's Internet Protocol address (e.g. IP address)</li>
+          <li>
+            The pages of the Application that you visit, the time and date of
+            your visit, the time spent on those pages
+          </li>
+          <li>The time spent on the Application</li>
+          <li>The operating system you use on your mobile device</li>
+        </ul>
+        <p className="mb-4">
+          The Application does not gather precise information about the location
+          of your mobile device.
+        </p>
+        <p className="mb-4">
+          The Application collects your device's location, which helps the
+          Service Provider determine your approximate geographical location and
+          make use of in below ways:
+        </p>
+        <ul className="list-disc pl-8 mb-4">
+          <li>
+            <strong>Geolocation Services</strong>: The Service Provider utilizes
+            location data to provide features such as personalized content,
+            relevant recommendations, and location-based services.
+          </li>
+          <li>
+            <strong>Analytics and Improvements</strong>: Aggregated and
+            anonymized location data helps the Service Provider to analyze user
+            behavior, identify trends, and improve the overall performance and
+            functionality of the Application.
+          </li>
+          <li>
+            <strong>Third-Party Services</strong>: Periodically, the Service
+            Provider may transmit anonymized location data to external services.
+            These services assist them in enhancing the Application and
+            optimizing their offerings.
+          </li>
+        </ul>
+        <p className="mb-4">
+          The Service Provider may use the information you provided to contact
+          you from time to time to provide you with important information,
+          required notices and marketing promotions.
+        </p>
+        <p className="mb-4">
+          For a better experience, while using the Application, the Service
+          Provider may require you to provide us with certain personally
+          identifiable information, including but not limited to{' '}
+          <a href="mailto:info@opalworld.com.sa" className="text-blue-600">
+            info@opalworld.com.sa
+          </a>
+          . The information that the Service Provider request will be retained
+          by them and used as described in this privacy policy.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Third Party Access</h3>
+        <p className="mb-4">
+          Only aggregated, anonymized data is periodically transmitted to
+          external services to aid the Service Provider in improving the
+          Application and their service. The Service Provider may share your
+          information with third parties in the ways that are described in this
+          privacy statement.
+        </p>
+        <p className="mb-4">
+          Please note that the Application utilizes third-party services that
+          have their own Privacy Policy about handling data. Below are the links
+          to the Privacy Policy of the third-party service providers used by the
+          Application:
+        </p>
+        <ul className="list-disc pl-8 mb-4">
+          <li>
+            <a
+              href="https://www.google.com/policies/privacy/"
+              className="text-blue-600"
+            >
+              Google Play Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://firebase.google.com/support/privacy/"
+              className="text-blue-600"
+            >
+              Google Analytics for Firebase
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://firebase.google.com/support/privacy/"
+              className="text-blue-600"
+            >
+              Firebase Crashlytics
+            </a>
+          </li>
+        </ul>
+        <p className="mb-4">
+          The Service Provider may disclose User Provided and Automatically
+          Collected Information:
+        </p>
+        <ul className="list-disc pl-8 mb-4">
+          <li>
+            as required by law, such as to comply with a subpoena, or similar
+            legal process;
+          </li>
+          <li>
+            when they believe in good faith that disclosure is necessary to
+            protect their rights, protect your safety or the safety of others,
+            investigate fraud, or respond to a government request;
+          </li>
+          <li>
+            with their trusted services providers who work on their behalf, do
+            not have an independent use of the information we disclose to them,
+            and have agreed to adhere to the rules set forth in this privacy
+            statement.
+          </li>
+        </ul>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Opt-Out Rights</h3>
+        <p className="mb-4">
+          You can stop all collection of information by the Application easily
+          by uninstalling it. You may use the standard uninstall processes as
+          may be available as part of your mobile device or via the mobile
+          application marketplace or network.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Data Retention Policy</h3>
+        <p className="mb-4">
+          The Service Provider will retain User Provided data for as long as you
+          use the Application and for a reasonable time thereafter. If you'd
+          like them to delete User Provided Data that you have provided via the
+          Application, please contact them at{' '}
+          <a href="mailto:info@opalworld.com.sa" className="text-blue-600">
+            info@opalworld.com.sa
+          </a>{' '}
+          and they will respond in a reasonable time.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Children</h3>
+        <p className="mb-4">
+          The Service Provider does not use the Application to knowingly solicit
+          data from or market to children under the age of 13.
+        </p>
+        <p className="mb-4">
+          The Application does not address anyone under the age of 13. The
+          Service Provider does not knowingly collect personally identifiable
+          information from children under 13 years of age. In the case the
+          Service Provider discover that a child under 13 has provided personal
+          information, the Service Provider will immediately delete this from
+          their servers. If you are a parent or guardian and you are aware that
+          your child has provided us with personal information, please contact
+          the Service Provider (
+          <a href="mailto:info@opalworld.com.sa" className="text-blue-600">
+            info@opalworld.com.sa
+          </a>
+          ) so that they will be able to take the necessary actions.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Security</h3>
+        <p className="mb-4">
+          The Service Provider is concerned about safeguarding the
+          confidentiality of your information. The Service Provider provides
+          physical, electronic, and procedural safeguards to protect information
+          the Service Provider processes and maintains.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Changes</h3>
+        <p className="mb-4">
+          This Privacy Policy may be updated from time to time for any reason.
+          The Service Provider will notify you of any changes to the Privacy
+          Policy by updating this page with the new Privacy Policy. You are
+          advised to consult this Privacy Policy regularly for any changes, as
+          continued use is deemed approval of all changes.
+        </p>
+        <p className="mb-4">
+          This privacy policy is effective as of <strong>2024-11-29</strong>.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Your Consent</h3>
+        <p className="mb-4">
+          By using the Application, you are consenting to the processing of your
+          information as set forth in this Privacy Policy now and as amended by
+          us.
+        </p>
+
+        <h3 className="text-xl font-bold mt-6 mb-4">Contact Us</h3>
+        <p className="mb-4">
+          If you have any questions regarding privacy while using the
+          Application, or have questions about the practices, please contact the
+          Service Provider via email at{' '}
+          <a href="mailto:info@opalworld.com.sa" className="text-blue-600">
+            info@opalworld.com.sa
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
+}

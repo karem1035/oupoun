adf818deac9af18e3305b69abc4d55c17991fa41
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-02-02 00:10:46 +0200

    language button change place

--- components/ScrollToTop.jsx
+++ components/ScrollToTop.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { ArrowUpCircle } from 'lucide-react';
+import { useLanguage } from '@/contexts/LanguageContext';
 
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = useState(false);
+  const { lang } = useLanguage();
 
   useEffect(() => {
     const toggleVisibility = () => {
@@ -27,7 +29,9 @@ export default function ScrollToTop() {
           exit={{ opacity: 0, scale: 0.8 }}
           transition={{ duration: 0.3 }}
           onClick={scrollToTop}
-          className="fixed bottom-6 right-6 bg-gray-700 hover:bg-gray-900 text-white p-3 rounded-full shadow-lg transition-all"
+          className={`fixed bottom-6  ${
+            lang === 'ar' ? 'left-6' : 'right-6'
+          } bg-gray-700 hover:bg-gray-900 text-white p-3 rounded-full shadow-lg transition-all`}
         >
           <ArrowUpCircle size={32} />
         </motion.button>
